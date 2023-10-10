41bdddfe11451c6a94582dd375f46be8ba25f206
anujsharma141 <19ucc109@lnmiit.ac.in>
2023-10-10 17:46:48 +0530

    fix: cors header controller added!

--- src/main/java/com/dashboard/dashboard/api/UserController.java
+++ src/main/java/com/dashboard/dashboard/api/UserController.java
@@ -2,6 +2,7 @@ package com.dashboard.dashboard.api;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dashboard.dashboard.model.User;
 import com.dashboard.dashboard.service.UserService;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 public class UserController {
 
