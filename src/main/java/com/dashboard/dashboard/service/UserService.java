606671f1c65051d61efbdf1225160aada944e646
anujsharma141 <19ucc109@lnmiit.ac.in>
2023-10-12 06:09:01 +0530

    fix: hide exposed user properties!

--- src/main/java/com/dashboard/dashboard/service/UserService.java
+++ src/main/java/com/dashboard/dashboard/service/UserService.java
@@ -1,6 +1,8 @@
 package com.dashboard.dashboard.service;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.dashboard.dashboard.model.User;
 
@@ -20,7 +22,14 @@ public class UserService {
     }
     
     public List<User> getUsers(){
-        return userRepository.findAll();
+        List<User> exposed = userRepository.findAll();
+        List<User> safe = exposed.stream().map(user->{
+            User safeUser = new User();
+            safeUser.setEmail(user.getEmail());
+            safeUser.setAge(user.getAge());
+            return safeUser;
+        }).collect(Collectors.toList());
+        return safe;
     }
 
     public void addUser(User user){
